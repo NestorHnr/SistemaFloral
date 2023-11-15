7c52795012ee2983ce423478eeaa187cca872544
Nestor Heredia <nestornhr27@hotmail.com>
2023-11-14 21:33:12 -0500

    Generar Reportes con Rotativa

--- SistemaFloral/obj/Debug/net7.0/SistemaFloral.MvcApplicationPartsAssemblyInfo.cs
+++ SistemaFloral/obj/Debug/net7.0/SistemaFloral.MvcApplicationPartsAssemblyInfo.cs
@@ -13,6 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.Identity.UI")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Rotativa.AspNetCore")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("SistemaFloral.AccesoDatos")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("SistemaFloral.Modelos")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("SistemaFloral.Utilidades")]
